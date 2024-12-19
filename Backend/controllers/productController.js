743992cbe93fbb669c399a0f100fdd31e4acedba
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-12-19 21:36:42 +0545

    product CRUD completed

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -46,3 +46,96 @@ imagePublicId = uploadResult.public_id
       res.status(500).json({ message: 'Internal server error' })
     }
   }
+
+
+  //fetch all products
+
+export const fetchAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+    res.status(200).json({data:products})
+  } catch (error) {
+    console.log("error in fetchAllProduct controller",error.message);
+    res.status(500).json({message: "Internal server error"})
+  }
+}
+
+
+//update a product
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const {title,description,category,brand,price,salePrice,totalStock,averageReview} = req.body;
+    const file=req.file
+    let product = await Product.findById(id);
+    if (!product){
+      return res.status(404).json({message:"Product not found"})
+    }
+  
+    let imageUrl = product.imageUrl
+    let imagePublicId = product.imagePublicId
+
+    if (file) {
+      // Delete old image from Cloudinary if it exists
+      if (product.imagePublicId) {
+        await cloudinary.uploader.destroy(product.imagePublicId);
+      }
+
+      // Upload new image to Cloudinary
+      const uploadResult = await cloudinary.uploader.upload(file.path, {
+        folder: 'E-Commerce',
+        transformation: [
+          { width: 300, height: 300, crop: "fill"}, // Example crop
+        ]
+      })
+
+      imageUrl = uploadResult.secure_url;
+      imagePublicId = uploadResult.public_id;
+    }
+   
+    const updatedProduct=await Product.findByIdAndUpdate(id,{
+        imageUrl,
+        imagePublicId,
+        title,
+        description,
+        category,
+        brand,
+        price,
+        salePrice,
+        totalStock,
+        averageReview,
+    },{new:true})
+
+    res.status(200).json({
+      message:"product updated successfully",
+      data:updatedProduct
+    })
+    
+  } catch (error) {
+    console.log("error in updateProduct controller",error.message);
+    res.status(500).json({message: "Internal server error"})
+  }
+}
+
+//deleteProduct
+export const deleteProduct=async(req,res)=>{
+  try {
+      const {id}=req.params
+      const product = await Product.findById(id);
+      if(!product){
+          return res.status(400).json({message:"product not available"})
+      }
+
+      // Delete image from Cloudinary
+    const publicId = product.imagePublicId; // Assuming your product model has `imagePublicId`
+    if (publicId) {
+      await cloudinary.uploader.destroy(publicId);
+    }
+      // Delete product from database
+      await Product.findByIdAndDelete(id)
+      res.status(200).json({ message: 'Product deleted successfully' })
+  } catch (error) {
+      console.log("Error in deleteBlog controller",error.message)
+      res.status(500).json({error:`Internal server error`})
+  }
+}
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addProduct } from "../controllers/productController.js"
+import { addProduct, deleteProduct, fetchAllProducts, updateProduct } from "../controllers/productController.js"
 import { upload } from "../utils/cloudinary.js"
 import isAuthenticated from "../middleware/isAuthenticated.js"
 import authorizeRole from "../middleware/roleMiddleware.js"
@@ -7,6 +7,9 @@ import authorizeRole from "../middleware/roleMiddleware.js"
 
 const router=express.Router()
 router.post('/addProduct',isAuthenticated,authorizeRole("admin"),upload,addProduct)
+router.get('/fetchAllProducts',isAuthenticated,fetchAllProducts)
+router.patch('/updateProduct/:id',isAuthenticated,authorizeRole("admin"),upload,updateProduct)
+router.delete('/deleteProduct/:id',isAuthenticated,authorizeRole("admin"),deleteProduct)
 
 
 export default router
